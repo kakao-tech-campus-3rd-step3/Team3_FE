920c17e089976d55cc2787d38f330fc339cd8aea
cheog0 <clf0914@naver.com>
2025-09-08 22:52:40 +0900

    refactor: 뒤로가기 컴포넌트 분리

--- src/components/ui/custom_header.tsx
+++ src/components/ui/custom_header.tsx
@@ -3,11 +3,29 @@ import { useNavigation } from '@react-navigation/native';
 import React, { useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 import { IconSymbol } from './icon_symbol';
+import { useRouter } from 'expo-router';
 
 interface CustomHeaderProps {
   title: string;
   showBackButton?: boolean;
 }
+
+const BackButton = () => {
+  const router = useRouter();
+
+  return (
+    <TouchableOpacity
+      onPress={() => router.back()}
+      style={{ marginLeft: theme.spacing.spacing4 }}
+    >
+      <IconSymbol
+        size={24}
+        color={theme.colors.text.main}
+        name="chevron.left"
+      />
+    </TouchableOpacity>
+  );
+};
 
 export const CustomHeader = ({
   title,
@@ -26,20 +44,7 @@ export const CustomHeader = ({
       headerTitleStyle: {
         fontWeight: theme.typography.fontWeight.bold,
       },
-      headerLeft: showBackButton
-        ? () => (
-            <TouchableOpacity
-              onPress={() => navigation.goBack()}
-              style={{ marginLeft: theme.spacing.spacing4 }}
-            >
-              <IconSymbol
-                size={24}
-                color={theme.colors.text.main}
-                name="chevron.left"
-              />
-            </TouchableOpacity>
-          )
-        : undefined,
+      headerLeft: showBackButton ? BackButton : undefined,
     });
   }, [navigation, title, showBackButton]);
 
