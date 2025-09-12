24792e2ed90a7dde5ef9a943efffb1aad26988d7
cheog0 <clf0914@naver.com>
2025-09-12 13:26:37 +0900

    feat: 인증 API 구현

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,12 +1,11 @@
 import { apiClient } from '@/src/lib/api_client';
-import { LoginResponse } from '@/src/types/auth';
+import { LoginResponse } from '@/src/types';
 import { AUTH_API } from '@/src/constants/endpoints';
 
 export const authApi = {
   login: (email: string, password: string) =>
-    apiClient.post<LoginResponse>(AUTH_API.GET_AUTH, {
+    apiClient.post<LoginResponse>(AUTH_API.LOGIN, {
       email,
       password,
     }),
-  getAuth: () => apiClient.get<LoginResponse>(AUTH_API.GET_AUTH),
 };
