fb5ead4cd701696e23a03b7c5914233a79014a8e
cheog0 <clf0914@naver.com>
2025-09-13 23:08:39 +0900

    feat: 팀 API 호출 훅 추가

--- src/hooks/queries.ts
+++ src/hooks/queries.ts
@@ -1,6 +1,11 @@
 import * as api from '@/src/api';
-import { useQuery } from '@tanstack/react-query';
-
+import { useQuery, useMutation } from '@tanstack/react-query';
+import type {
+  JoinTeamResponse,
+  CreateTeamResponse,
+  CreateTeamRequest,
+} from '@/src/types';
+import { queryClient } from '@/src/lib/query_client';
 export const queries = {
   userProfile: {
     key: ['user', 'profile'] as const,
@@ -14,6 +19,10 @@ export const queries = {
   recommendedMatch: {
     key: ['recommendedMatch'] as const,
     fn: () => api.recommendedMatchApi.getRecommendedMatch(),
+  },
+  universityTeamList: {
+    key: ['university'] as const,
+    fn: () => api.universityListApi.getUniversities(),
   },
 } as const;
 
@@ -35,5 +44,51 @@ export function useRecommendedMatch() {
   return useQuery({
     queryKey: queries.recommendedMatch.key,
     queryFn: queries.recommendedMatch.fn,
+  });
+}
+
+export function useUniversityTeamList() {
+  return useQuery({
+    queryKey: queries.universityTeamList.key,
+    queryFn: queries.universityTeamList.fn,
+  });
+}
+
+export function useCreateTeamMutation() {
+  return useMutation({
+    mutationFn: async (
+      teamData: CreateTeamRequest
+    ): Promise<CreateTeamResponse> => {
+      const data: CreateTeamResponse =
+        await api.createTeamApi.createTeam(teamData);
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: queries.userProfile.key });
+    },
+    onError: (error: unknown) => {
+      console.error('팀 생성 실패:', error);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: queries.userProfile.key });
+    },
+  });
+}
+
+export function useJoinTeamMutation() {
+  return useMutation({
+    mutationFn: async (teamId: number): Promise<JoinTeamResponse> => {
+      const data: JoinTeamResponse = await api.joinTeamApi.joinTeam(teamId);
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: queries.userProfile.key });
+    },
+    onError: (error: unknown) => {
+      console.error('팀 참여 실패:', error);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: queries.userProfile.key });
+    },
   });
 }
