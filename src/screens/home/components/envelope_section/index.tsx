28ec970d9ef2e973c7b8ef843513bceb1da6bb92
jskim3936 <jskim3936@naver.com>
2025-09-19 20:20:33 +0900

    feat: 매치 생성 라우팅 설정

--- src/screens/home/components/envelope_section/index.tsx
+++ src/screens/home/components/envelope_section/index.tsx
@@ -1,6 +1,7 @@
 import { Ionicons } from '@expo/vector-icons';
+import { router } from 'expo-router';
 import { memo } from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 
 import { theme } from '@/src/theme';
 
@@ -9,21 +10,25 @@ import styles from '../../home_style';
 export default memo(function EnvelopeSection() {
   return (
     <View style={styles.envelopeSection}>
-      <View style={styles.envelopeHeader}>
-        <View style={styles.envelopeIcon}>
-          <Image
-            source={require('@/assets/images/apply.png')}
-            style={{ width: 20, height: 20 }}
+      <TouchableOpacity
+        onPress={() => router.push('/match_making/teammate_register')}
+      >
+        <View style={styles.envelopeHeader}>
+          <View style={styles.envelopeIcon}>
+            <Image
+              source={require('@/assets/images/apply.png')}
+              style={{ width: 20, height: 20 }}
+            />
+          </View>
+          <Text style={styles.envelopeTitle}>매치 신청하기</Text>
+
+          <Ionicons
+            name="chevron-forward"
+            size={20}
+            color={theme.colors.text.sub}
           />
         </View>
-        <Text style={styles.envelopeTitle}>매치 신청하기</Text>
-
-        <Ionicons
-          name="chevron-forward"
-          size={20}
-          color={theme.colors.text.sub}
-        />
-      </View>
+      </TouchableOpacity>
     </View>
   );
 });
