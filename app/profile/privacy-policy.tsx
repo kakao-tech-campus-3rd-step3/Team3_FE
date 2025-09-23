537ab10ca25964214e1a3390f014b9696267af2b
YuJun Seol <clf0914@naver.com>
2025-09-23 13:33:11 +0900

    refactor: profile-privacy-info (#20)
    * feat: 외부 링크 상수 관리 추가
    
    * remove: 개인정보처리방침 및 서비스이용약관 화면 파일 삭제
    
    * refactor: 설정 항목에서 외부 링크 직접 연결

--- app/profile/privacy-policy.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import PrivacyPolicyScreen from '@/src/screens/profile/privacy_policy/privacy_policy_screen';
-
-export default function PrivacyPolicyRoute() {
-  return <PrivacyPolicyScreen />;
-}
