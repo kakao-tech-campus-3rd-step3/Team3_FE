910a4a697d6d821d17abf425dc156c82ab8ad2d6
cheog0 <clf0914@naver.com>
2025-09-25 00:27:17 +0900

    fix: 사용하지 않는 import 삭제

--- src/screens/home/components/greeting_section/index.tsx
+++ src/screens/home/components/greeting_section/index.tsx
@@ -1,7 +1,6 @@
 import { memo } from 'react';
 import { Text, View } from 'react-native';
 
-import { theme } from '@/src/theme';
 import { HomeData } from '@/src/types/home';
 import { UserProfile } from '@/src/types/profile';
 
