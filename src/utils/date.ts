ff85b4db4d4493652b4adf08fa87d77c955aa277
cheog0 <clf0914@naver.com>
2025-09-27 00:27:45 +0900

    feat: 날짜/시간 포맷팅 함수 유틸리티 함수 추가

---
+++ src/utils/date.ts
@@ -0,0 +1,9 @@
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  return `${date.getFullYear()}.${String(date.getMonth() + 1).padStart(2, '0')}.${String(date.getDate()).padStart(2, '0')}`;
+};
+
+export const formatTime = (timeString: string) => {
+  const [hours, minutes] = timeString.split(':');
+  return `${hours}:${minutes}`;
+};
