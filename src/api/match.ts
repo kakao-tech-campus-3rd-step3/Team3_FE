e70ed8f246a1fbb661bf8f85233a4bc6c8546d4f
cheog0 <clf0914@naver.com>
2025-10-01 01:06:34 +0900

    feat(types): 매치 관련 타입 정의 추가

--- src/api/match.ts
+++ src/api/match.ts
@@ -1,11 +1,17 @@
 import { TEAM_MATCH_API } from '@/src/constants/endpoints';
 import { apiClient } from '@/src/lib/api_client';
-import { Match } from '@/src/types/match';
+import { Match, RecentMatchResponse } from '@/src/types/match';
 
 export const teamMatchApi = {
   getTeamMatches: (teamId: string | number) => {
     return apiClient.get<{ [key: string]: Match[] }>(
       TEAM_MATCH_API.GET_TEAM_RECENT_MATCHES(teamId)
     );
+  },
+  getTeamRecentMatches: (teamId: string | number, status?: string) => {
+    const url = status
+      ? `${TEAM_MATCH_API.GET_TEAM_RECENT_MATCHES(teamId)}?status=${status}`
+      : TEAM_MATCH_API.GET_TEAM_RECENT_MATCHES(teamId);
+    return apiClient.get<RecentMatchResponse[]>(url);
   },
 };
