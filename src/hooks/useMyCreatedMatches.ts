6b5465cf25f7317f982a166cee45504b491efdc0
jskim <jskim3936@naver.com>
2025-10-06 00:53:12 +0900

    refactor: useMyCreatedMatches 훅 타입 수정

--- src/hooks/useMyCreatedMatches.ts
+++ src/hooks/useMyCreatedMatches.ts
@@ -1,10 +1,15 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 
 import { getMyCreatedMatches } from '@/src/api/match';
 import type { MatchWaitingResponseDto } from '@/src/types/match';
 
-export const useMyCreatedMatches = (options?: any) => {
-  return useQuery<MatchWaitingResponseDto[]>({
+export const useMyCreatedMatches = (
+  options?: UseQueryOptions<
+    MatchWaitingResponseDto[], // data type
+    Error // error type
+  >
+) => {
+  return useQuery<MatchWaitingResponseDto[], Error>({
     queryKey: ['my-created-matches'],
     queryFn: async () => {
       const result = await getMyCreatedMatches();
