1c0bb9ac72ded6e649a0e425ab94f656f4dec461
cheog0 <clf0914@naver.com>
2025-10-31 21:56:04 +0900

    refactor: 코드 일관성을 위하여 절대경로로 수정

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,18 +1,18 @@
-import { colors } from './colors';
-import { spacing } from './spacing';
-import { typography } from './typography';
+import { colors } from '@/src/theme/colors';
+import { spacing } from '@/src/theme/spacing';
+import { typography } from '@/src/theme/typography';
 
-export { colors } from './colors';
-export { spacing } from './spacing';
-export { typography } from './typography';
+export { colors } from '@/src/theme/colors';
+export { spacing } from '@/src/theme/spacing';
+export { typography } from '@/src/theme/typography';
 
-export type { ColorShade, ColorVariant } from './colors';
-export type { SpacingSize, SpacingValue } from './spacing';
+export type { ColorShade, ColorVariant } from '@/src/theme/colors';
+export type { SpacingSize, SpacingValue } from '@/src/theme/spacing';
 export type {
   TypographySize,
   TypographyText,
   TypographyWeight,
-} from './typography';
+} from '@/src/theme/typography';
 
 export const theme = {
   colors,
